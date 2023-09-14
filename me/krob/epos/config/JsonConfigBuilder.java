c51eb93885b4b997c1f87419de6f446c1f861606
kylerob25 <kylrob25@icloud.com>
2023-09-14 10:04:09 +0100

    Add JsonConfigBuilder.java

---
+++ me/krob/epos/config/JsonConfigBuilder.java
@@ -0,0 +1,49 @@
+package me.krob.epos.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.nio.file.Files;
+
+public class JsonConfigBuilder<C> {
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+    private final Class<C> clazz;
+
+    private File file;
+
+    public JsonConfigBuilder(Class<C> clazz, String fileName) {
+        this.clazz = clazz;
+        this.file = new File(fileName);
+    }
+
+    public void makeParent() {
+        File parent = file.getParentFile();
+        if (!parent.exists()) parent.mkdirs();
+    }
+
+    public C loadFile() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(Files.newInputStream(file.toPath())))) {
+            return (C) GSON.fromJson(reader, clazz);
+        } catch (IOException exception) {
+            return createFile();
+        }
+    }
+
+    public C createFile() {
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(toJsonString());
+            writer.flush();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+            return null;
+        }
+
+        return loadFile();
+    }
+
+    public String toJsonString() throws Throwable {
+        return GSON.toJson(clazz.getDeclaredConstructor().newInstance());
+    }
+}
--- me/krob/epos/util/KeypadHandler.java
+++
@@ -1,7 +0,0 @@
-package me.krob.epos.util;
-
-import javax.swing.*;
-
-public class KeypadHandler {
-    private JButton one, two, three, four, five, six, seven, eight, nine, zero, clear, enter;
-}
